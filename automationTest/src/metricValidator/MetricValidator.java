e5556b8b443e997a952ba059737b4eae1d6bf486
mubari <mubari@3esi.com>
2015-12-21 16:09:05 -0700

    Now able to validate metrics.
    Must provide WebElement of dashboard metric to validate

--- automationTest/src/metricValidator/MetricValidator.java
+++ automationTest/src/metricValidator/MetricValidator.java
@@ -9,10 +9,13 @@ import java.text.DecimalFormat;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.Reporter;
+
+import seleniumTools.WebDriverTools;
 
 import excelUtils.ExcelUtils;
 
@@ -46,59 +49,83 @@ public class MetricValidator
 	//------------------------------SETTERS-----------------------------------//
 	//------------------------------METHODS-----------------------------------//
 	
+	/**
+	 * @param driver
+	 * @param metric
+	 * @param value
+	 * @param excelFileLocation
+	 * @param sheetName
+	 * @param metricName
+	 * @param row
+	 * @param column
+	 * @param units
+	 * @param wait_time
+	 * @throws IOException
+	 * @throws InterruptedException 
+	 */
 	public static void validateMetric(WebDriver driver,
 									  WebElement metric,
-									  String value, //to be removed once able to detect web objects dynamically
 									  String excelFileLocation,
 									  String sheetName,
+									  Object[] validateStatus,
 									  String metricName,
 									  int row, 
 									  int column,
 									  String units,
-									  long wait_time) throws IOException
+									  long wait_time) throws IOException, InterruptedException
 	{
 		String reporterFontFace = "'verdana'";
 		String reporterNewLine = "<br>";
 		ExcelUtils.setExcelFile(new File(excelFileLocation).getCanonicalPath(), sheetName);
-		String metricValue = getMetricValue(row, column);
-		String metricUnit = null;
+		String metricValueExcel = getMetricValue(row, column);
+		String metricValueDashboard = metric.findElement(By.cssSelector("h2.no-margins.ng-scope")).getText();
+		String screenShotPath = "res\\screenshots\\TestNG_report\\" + System.getProperty("testNG_screenshotDir");
 		
-		
-		Reporter.log("<blockquote>");
+		((StringBuilder) validateStatus[1]).append("<blockquote>");
 		
 		try
 		{
-			Reporter.log("<font face=" + reporterFontFace + ">Metric value in excel: " + metricValue + "</font>" + reporterNewLine);
-			Reporter.log("<font face=" + reporterFontFace + ">Metric value in dashboard: " + metricName + "</font>" + reporterNewLine);
-			Reporter.log("<font face=" + reporterFontFace + ">Checking metric value in excel and in dashboard are equal</font>" + reporterNewLine);
+			((StringBuilder) validateStatus[1]).append("<font face=" + reporterFontFace + ">Metric value in excel: " + metricValueExcel + (units != null ? " " + units : "") + "</font>" + reporterNewLine);
+			((StringBuilder) validateStatus[1]).append("<font face=" + reporterFontFace + ">Metric value in dashboard: " + metricValueDashboard + "</font>" + reporterNewLine);
+			((StringBuilder) validateStatus[1]).append("<font face=" + reporterFontFace + ">Checking metric value in excel and in dashboard are equal</font>" + reporterNewLine);
 			//Assert.assertEquals(metricValue + " ", value + " " units);
 			if(units != null)
 			{
-				Assert.assertEquals(metricValue + " ", value + " " + units);
+				Assert.assertEquals(metricValueExcel + " " + units, metricValueDashboard);
 				
 			}//END if(units != null)
 			
 			else
 			{
-				Assert.assertEquals(metricValue, value);
+				Assert.assertEquals(metricValueExcel, metricValueDashboard);
 				
 			}//END ELSE FOR if(units != null)
 			
-			Reporter.log("<font face=" + reporterFontFace + " color='green'><b>Metric value are equal</b></font>" + reporterNewLine);
+			validateStatus[0] = true;
+			((StringBuilder) validateStatus[1]).append("<font face=" + reporterFontFace + " color='green'><b>Metric value are equal</b></font>" + reporterNewLine);
 			
 		}//END TRY BLOCK
 		
 		catch(AssertionError e)
 		{
-			Reporter.log("<blockquote>");
-			Reporter.log("<font face=" + reporterFontFace + " color='red'>Metric value are <b>NOT</b> equal" + reporterNewLine);
-			Reporter.log("</blockquote>");
-			Reporter.log("</blockquote>");
-			Reporter.log("<hr>" + reporterNewLine);
+			validateStatus[0] = false;
+			((StringBuilder) validateStatus[1]).append("<blockquote>");
+			((StringBuilder) validateStatus[1]).append("<font face=" + reporterFontFace + " color='red'>Metric value are <b>NOT</b> equal" + reporterNewLine);
+			
+			Thread.sleep(wait_time);
+			String screenshotName = "screenshot";
+			WebDriverTools.takeScreenshot(driver, screenShotPath, screenshotName);
+			String webImageLink = "..\\..\\..\\" + screenShotPath + "\\" + screenshotName + "_" + (WebDriverTools.getScreenShotNumber() - 1) + ".jpg";
+			//screenShotPath + "\\" + screenshotName + "_" + screenShotNumber++ + ".jpg
+			((StringBuilder) validateStatus[1]).append("<a target='_blank' href='" + webImageLink + "'><img src='" + webImageLink + "' style='width:25%; height:25%'></a> <br>");
+			
+			((StringBuilder) validateStatus[1]).append("</blockquote>");
+			((StringBuilder) validateStatus[1]).append("</blockquote>");
+			((StringBuilder) validateStatus[1]).append("<hr>" + reporterNewLine);
 			
 			if(exitOnFail == true)
-			{
-				throw new AssertionError("Metric value is not equal: Metric in dashboard: " + value + " . Metric in excel: " + metricValue);
+			{ 
+				throw new AssertionError("Metric value is not equal: Metric in dashboard: " + metricValueDashboard + " . Metric in excel: " + metricValueExcel + (units != null ? " " + units: ""));
 				
 			}
 			
@@ -106,7 +133,7 @@ public class MetricValidator
 			
 		}//END CATCH BLOCK
 		
-		Reporter.log("</blockquote>");
+		((StringBuilder) validateStatus[1]).append("</blockquote>");
 		
 	}//END METHOD validateMetric(WebElement, String, String, int, int, String, long)
 	
